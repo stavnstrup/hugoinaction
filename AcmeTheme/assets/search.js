e884b8b28c06cc8d2113c888d1c1b8a6fb4b47f6
Atishay Jain <contact@atishay.me>
2021-03-29 22:49:30 -0700

    10.5.7: Properly showing search results.

--- AcmeTheme/assets/search.js
+++ AcmeTheme/assets/search.js
@@ -1,6 +1,7 @@
 import Fuse from 'fuse.js'
 
 let index = null;
+const MAX_SEARCH_RESULTS = 5;
 export default {
   async init() {
     try {
@@ -21,11 +22,54 @@ export default {
           name: 'content'
         }]
       });
-      // Just to test. Do not leave in code.
-      console.log(index.search('acme'));
+
+      document.addEventListener("keyup", this.onKeyUp);
+      document.addEventListener("input", this.showResults);
+      document.addEventListener("focusin", this.showResults);
+      document.addEventListener("focusout", this.onLoseFocus);
     } catch (e) {
       this.removeSearch();
     }
+  },
+
+  showResults(event) {
+    const searchBox = document.querySelector("#search input");
+    if (event.target !== searchBox) {
+      return;
+    }
+    const result = document.querySelector("#search div");
+    result.style.display = "block";
+    if (searchBox.value.length > 0) {
+      const results = index.search(searchBox.value);
+      result.innerHTML = results
+        .slice(0, MAX_SEARCH_RESULTS)
+        .map(x => `<a href="${x.item.url}">${x.item.title}</a>`)
+        .join("");
+    } else {
+      result.innerHTML = '';
+    }
+  },
+
+  onKeyUp(event) {
+    const searchBox = document.querySelector("#search input");
+    if (event.target !== searchBox) {
+      return;
+    }
+    if (event.key === "Enter") {
+      const results = index.search(searchBox.value);
+      if (results.length > 0) {
+        window.location = results[0].item.url;
+      }
+    }
+  },
+
+  onLoseFocus(event) {
+    const searchBox = document.querySelector("#search input");
+    if (event.target !== searchBox) {
+      return;
+    }
+    const result = document.querySelector("#search div");
+    if (!event.relatedTarget.matches("#search div *")) { result.style.display = "none"; }
   },
 
   removeSearch() {
